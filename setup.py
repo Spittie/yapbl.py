4b2e40beded0a362208015fa91ddd3f4fcb71f1d
Kabir Sala <spittiepie@gmail.com>
2014-06-02 00:10:25 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.1',
     packages=['yapbl'],
     url='https://github.com/Spittie/yapbl.py',
+    downloadurl='https://github.com/Spittie/yapbl.py/archive/0.1.tar.gz',
     license='MIT',
     author='Spittie',
     author_email='spittiepie@gmail.com',
